fcaa9de5b0360ac6f34991f3b26537ed2d95a2d6
TGashnov <91610180+TGashnov@users.noreply.github.com>
2021-10-03 00:07:14 +0400

    Add files via upload

---
+++ Program1_1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task1._1
+{
+    class Program1_1
+    {
+        static void Main(string[] args)
+        {
+            Check(out int number);
+            Console.WriteLine(Math.Pow(number, 2));
+
+            Console.ReadKey();
+        }
+
+        static void Check(out int number)
+        {
+            Console.WriteLine("Введите число от -1000000 до 1000000");
+            number = int.Parse(Console.ReadLine());
+            while (number < -1000000 || number > 1000000)
+            {
+                Console.WriteLine("Вы ввели неправильное число");
+                number = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
---
+++ Program1_2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task1._2
+{
+    class Program1_2
+    {
+        static void Main(string[] args)
+        {
+            Solve();
+
+            Console.ReadKey();
+        }
+
+        static void Solve()
+        {
+            Console.WriteLine("Введите что-нибудь");
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out _))
+            {
+                Console.WriteLine("Вы ввели целое число");
+            }
+            else if (double.TryParse(input, out _))
+            {
+                Console.WriteLine("Вы ввели рациональное число");
+            }
+            else if (bool.TryParse(input, out _))
+            {
+                Console.WriteLine("Вы ввели логическое значение");
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели текст");
+            }
+        }
+    }
+}
